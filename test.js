f4b434a77ead137e48661e4dbb95cf25d91f085d
Cihat Altuntaş <cihat.altuntas@ericsson.com>
2019-03-14 16:29:59 +0300

    initial commit

---
+++ test.js
@@ -0,0 +1,48 @@
+const cheerio = require('cheerio');
+const http = require('https');
+
+const BASE_STATS_URL = 'https://finance.yahoo.com/quote/';
+const TICKERS_URL = 'https://borsa.doviz.com/hisseler';
+
+async function httpRequest(url) {
+    return new Promise(function (resolve, reject) {
+        var request = http.request(url, function (res) {
+            var data = '';
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                resolve(data);
+            });
+        });
+
+        request.on('error', function (e) {
+            reject(e);
+        });
+        request.end();
+    });
+}
+
+async function getTickers() {
+    const data = await httpRequest(TICKERS_URL);
+    const $ = cheerio.load(data);
+    let tickers = [];
+    $('#stocks  > tbody > tr > td > a').each(function (i, element) {
+        const text = element.childNodes[0].data;
+        tickers.push(text);
+    });
+
+    for (let index = 0; index < tickers.length; index++) {
+        const t = tickers[index];
+        await getStatistics(t + '.IS');
+    }
+}
+
+async function getStatistics(ticker) {
+    const data = await httpRequest(BASE_STATS_URL + ticker + '/key-statistics?p=' + ticker);
+    const $ = cheerio.load(data);
+    var ev_ebitda = $('span:contains("Enterprise Value/EBITDA")').parent().next().html()
+    console.log(ticker + ':' + ev_ebitda);
+}
+
+getTickers();
